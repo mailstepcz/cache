b3e33540d49f2f426f79f3314339ecc9d83945e7
Petr Homola <homola.p@gmail.com>
2025-01-28 09:54:28 +0000

    object cache

--- cache.go
+++ cache.go
@@ -1,9 +1,17 @@
 package cache
 
 import (
+	"bytes"
+	"encoding/gob"
+	"errors"
 	"runtime"
 	"sync"
 	"weak"
+)
+
+var (
+	// ErrCacheMiss indicates a cache miss.
+	ErrCacheMiss = errors.New("cache miss")
 )
 
 // Cache is a cache for transient values.
@@ -15,6 +23,9 @@ type Cache[K comparable, V any] struct {
 func (c *Cache[K, V]) Put(key K, value *V) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
+	if c.data == nil {
+		c.data = make(map[K]weak.Pointer[V])
+	}
 	c.data[key] = weak.Make(value)
 	runtime.AddCleanup(value, func(key K) {
 		c.lock.Lock()
@@ -32,4 +43,33 @@ func (c *Cache[K, V]) Get(key K) (*V, bool) {
 		}
 	}
 	return nil, false
+}
+
+type cachedObject struct {
+	data []byte
+}
+
+// ObjectCache is a cache for transient objects.
+type ObjectCache[K comparable, V any] struct {
+	data Cache[K, cachedObject]
+}
+
+func (c *ObjectCache[K, V]) Put(key K, object *V) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(object); err != nil {
+		return err
+	}
+	c.data.Put(key, &cachedObject{buf.Bytes()})
+	return nil
+}
+
+func (c *ObjectCache[K, V]) Get(key K) (*V, error) {
+	if co, ok := c.data.Get(key); ok {
+		var obj V
+		if err := gob.NewDecoder(bytes.NewReader(co.data)).Decode(&obj); err != nil {
+			return nil, err
+		}
+		return &obj, nil
+	}
+	return nil, ErrCacheMiss
 }
--- cache_test.go
+++ cache_test.go
@@ -1,1 +1,26 @@
 package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestObjectCache(t *testing.T) {
+	req := require.New(t)
+
+	type person struct {
+		Name string
+		Age  int
+	}
+
+	var c ObjectCache[string, person]
+
+	err := c.Put("p1", &person{"Oisin", 18})
+	req.NoError(err)
+
+	p, err := c.Get("p1")
+	req.NoError(err)
+	req.Equal("Oisin", p.Name)
+	req.Equal(18, p.Age)
+}
