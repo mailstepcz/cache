033618c427d687cd1d0b8086b6acf9a7bef42b14
Petr Homola <homola.p@gmail.com>
2025-01-30 13:37:42 +0000

    cleanup stopping

--- cache.go
+++ cache.go
@@ -12,20 +12,32 @@ var (
 	ErrCacheMiss = errors.New("cache miss")
 )
 
+type cacheObject[T any] struct {
+	ptr     weak.Pointer[T]
+	cleanup runtime.Cleanup
+}
+
 // Cache is a cache for transient values.
 type Cache[K comparable, V any] struct {
 	lock sync.RWMutex
-	data map[K]weak.Pointer[V]
+	data map[K]*cacheObject[V]
 }
 
 func (c *Cache[K, V]) Put(key K, value *V) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
+
 	if c.data == nil {
-		c.data = make(map[K]weak.Pointer[V])
+		c.data = make(map[K]*cacheObject[V])
 	}
-	c.data[key] = weak.Make(value)
-	runtime.AddCleanup(value, func(key K) {
+
+	if obj, ok := c.data[key]; ok {
+		obj.cleanup.Stop()
+	}
+
+	obj := &cacheObject[V]{ptr: weak.Make(value)}
+	c.data[key] = obj
+	obj.cleanup = runtime.AddCleanup(value, func(key K) {
 		c.lock.Lock()
 		defer c.lock.Unlock()
 		delete(c.data, key)
@@ -35,8 +47,8 @@ func (c *Cache[K, V]) Put(key K, value *V) {
 func (c *Cache[K, V]) Get(key K) (*V, bool) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
-	if ptr, ok := c.data[key]; ok {
-		if value := ptr.Value(); value != nil {
+	if obj, ok := c.data[key]; ok {
+		if value := obj.ptr.Value(); value != nil {
 			return value, true
 		}
 	}
