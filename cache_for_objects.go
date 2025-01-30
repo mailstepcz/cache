5beeeda2e04f8a585f367013d9eab875bee8f31b
Petr Homola <homola.p@gmail.com>
2025-01-30 13:29:41 +0000

    split file

---
+++ cache_for_objects.go
@@ -0,0 +1,35 @@
+package cache
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+type ocachedObject struct {
+	data []byte
+}
+
+// ObjectCache is a cache for transient objects.
+type ObjectCache[K comparable, V any] struct {
+	data Cache[K, ocachedObject]
+}
+
+func (c *ObjectCache[K, V]) Put(key K, object *V) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(object); err != nil {
+		return err
+	}
+	c.data.Put(key, &ocachedObject{buf.Bytes()})
+	return nil
+}
+
+func (c *ObjectCache[K, V]) Get(key K) (*V, error) {
+	if co, ok := c.data.Get(key); ok {
+		var obj V
+		if err := gob.NewDecoder(bytes.NewReader(co.data)).Decode(&obj); err != nil {
+			return nil, err
+		}
+		return &obj, nil
+	}
+	return nil, ErrCacheMiss
+}
