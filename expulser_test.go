0f890a86fa6cf1a811e923b188d4c3c0b7c6b78c
Petr Homola <phomola@me.com>
2025-02-25 22:02:47 +0000

    Expulser tests

--- expulser_test.go
+++ expulser_test.go
@@ -1,1 +1,24 @@
 package cache
+
+import (
+	"runtime"
+	"testing"
+
+	"github.com/mailstepcz/testutils/testcond"
+)
+
+func TestExpulser(t *testing.T) {
+	type person struct {
+		name string
+		age  int
+	}
+
+	var ex expulser[person]
+
+	var expelled bool
+	ex.add(&person{"Saoirse", 25}, func() {
+		expelled = true
+	})
+	runtime.GC()
+	testcond.Equal(t, true, expelled)
+}
