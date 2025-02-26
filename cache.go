b898a9a75e63b32c77c6a9a9ee3dad9f62ea2a3c
OndrejBelza <32128791+OndrejBelza@users.noreply.github.com>
2025-02-26 12:02:53 +0100

    feat: nil timer fix and cache value finalizer (#7)
    * fix: sync map implementation fix
    
    * feat: refactor

--- cache.go
+++ cache.go
@@ -1,24 +1,93 @@
 package cache
 
 import (
+	"iter"
 	"runtime"
 	"sync"
 	"time"
 	"unsafe"
 )
 
-// TransientPtr is a transient pointer.
-type TransientPtr uintptr
+type innerTransientPtr[T any] interface {
+	Pointer() *T
+}
 
-// UnsafePointer returns the pointer as an unsafe pointer.
-func (tp TransientPtr) UnsafePointer() unsafe.Pointer {
-	return unsafe.Pointer(tp)
+type innerTransientPtrGC[T any] struct {
+	mtx sync.RWMutex
+	ptr uintptr
+}
+
+func (ptr *innerTransientPtrGC[T]) Pointer() *T {
+	ptr.mtx.RLock()
+	defer ptr.mtx.RUnlock()
+	return *(**T)(unsafe.Pointer(&ptr.ptr))
+}
+
+type innerTransientPtrExp[T any] struct {
+	mtx sync.RWMutex
+	ptr *T
+}
+
+func (ptr *innerTransientPtrExp[T]) Pointer() *T {
+	ptr.mtx.RLock()
+	defer ptr.mtx.RUnlock()
+	return ptr.ptr
+}
+
+// transientPtr is a transient pointer.
+type transientPtr[T any] struct {
+	ptr innerTransientPtr[T]
+}
+
+type deleteCallback = func()
+
+func makeTransientPtr[T any](p *T, cbs ...deleteCallback) transientPtr[T] {
+	innerPtr := &innerTransientPtrGC[T]{
+		ptr: uintptr(unsafe.Pointer(p)),
+	}
+
+	runtime.AddCleanup(p, func(ptr *innerTransientPtrGC[T]) {
+		for _, cb := range cbs {
+			cb()
+		}
+		ptr.mtx.Lock()
+		defer ptr.mtx.Unlock()
+		ptr.ptr = 0
+	}, innerPtr)
+
+	return transientPtr[T]{
+		ptr: innerPtr,
+	}
+
+}
+
+func makeTransientPtrExpiring[T any](p *T, exp time.Duration, cbs ...deleteCallback) transientPtr[T] {
+	innerPtr := &innerTransientPtrExp[T]{
+		ptr: p,
+	}
+
+	time.AfterFunc(exp, func() {
+		for _, cb := range cbs {
+			cb()
+		}
+		innerPtr.mtx.Lock()
+		defer innerPtr.mtx.Unlock()
+		innerPtr.ptr = nil
+	})
+
+	return transientPtr[T]{
+		ptr: innerPtr,
+	}
+}
+
+// Pointer returns the pointer as an unsafe pointer.
+func (tp transientPtr[T]) Pointer() *T {
+	return tp.ptr.Pointer()
 }
 
 type cacheObject[T any] struct {
-	ptr     TransientPtr
-	timer   *time.Timer
-	version int
+	ptr       transientPtr[T]
+	timestamp time.Time
 }
 
 // Cache is a cache for transient values.
@@ -26,43 +95,63 @@ type Cache[K comparable, V any] struct {
 	data sync.Map
 }
 
+// Put puts new value into the cache.
 func (c *Cache[K, V]) Put(key K, value *V) *cacheObject[V] {
-	var ver int
-	if obj, ok := c.data.Load(key); ok {
-		obj := obj.(*cacheObject[V])
-		obj.timer.Stop()
-		ver = obj.version + 1
-	}
+	timestamp := time.Now()
 
-	obj := &cacheObject[V]{ptr: TransientPtr(unsafe.Pointer(value)), version: ver}
+	obj := &cacheObject[V]{ptr: makeTransientPtr(value, func() {
+		if obj, ok := c.data.Load(key); ok {
+			obj := obj.(*cacheObject[V])
+			if timestamp.Equal(obj.timestamp) {
+				c.data.Delete(key)
+			}
+		}
+	}), timestamp: timestamp}
+
 	c.data.Store(key, obj)
-	runtime.SetFinalizer(value, func(_ *V) {
-		/* use 'add cleanup' here once available
-		go func() {
-			c.data.Delete(key)
-		}()*/
-	})
 
 	return obj
 }
 
+// PutExpiring puts new value to the cache. Value will be removed after specified expiration.
 func (c *Cache[K, V]) PutExpiring(key K, value *V, exp time.Duration) {
-	obj := c.Put(key, value)
-	obj.timer = time.AfterFunc(exp, func() {
-		c.data.Delete(key)
-	})
+	timestamp := time.Now()
+
+	obj := &cacheObject[V]{ptr: makeTransientPtrExpiring(value, exp, func() {
+		if obj, ok := c.data.Load(key); ok {
+			obj := obj.(*cacheObject[V])
+			if timestamp.Equal(obj.timestamp) {
+				c.data.Delete(key)
+			}
+		}
+
+	}), timestamp: timestamp}
+
+	c.data.Store(key, obj)
 }
 
+// PutValue inserts new value to the cache. And returns inserted value.
 func (c *Cache[K, V]) PutValue(key K, value V) V {
 	c.Put(key, &value)
 	return value
 }
 
+// Get returns value in cache and flag which indicates that value is available.
 func (c *Cache[K, V]) Get(key K) (*V, bool) {
 	if obj, ok := c.data.Load(key); ok {
 		obj := obj.(*cacheObject[V])
-		return (*V)(obj.ptr.UnsafePointer()), true
+		return obj.ptr.Pointer(), true
 	}
 
 	return nil, false
+}
+
+// GetAll returns all values stored in cache.
+func (c *Cache[K, V]) GetAll() iter.Seq2[K, *V] {
+	return func(yield func(K, *V) bool) {
+		c.data.Range(func(key, value any) bool {
+			return yield(key.(K), value.(*cacheObject[V]).ptr.Pointer())
+		})
+	}
+
 }
--- cache_test.go
+++ cache_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type person struct {
+	Name string
+	Age  int
+}
+
 func TestObjectCache(t *testing.T) {
 	req := require.New(t)
-
-	type person struct {
-		Name string
-		Age  int
-	}
 
 	var c ObjectCache[string, person]
 
@@ -21,6 +21,19 @@ func TestObjectCache(t *testing.T) {
 
 	p, err := c.Get("p1")
 	req.NoError(err)
+	req.Equal("Oisin", p.Name)
+	req.Equal(18, p.Age)
+}
+
+func TestCache(t *testing.T) {
+	req := require.New(t)
+
+	var c Cache[string, person]
+	val := c.Put("p1", &person{"Oisin", 18})
+	req.NotNil(val)
+
+	p, found := c.Get("p1")
+	req.True(found)
 	req.Equal("Oisin", p.Name)
 	req.Equal(18, p.Age)
 }
